7252ad3a1b5376607fab2a7ef37738fe44c40383
sadiali97 <smartchhipa64@gmail.com>
2022-07-20 11:03:43 +0500

    updated

--- index.js
+++ index.js
@@ -1,35 +1,66 @@
-class car{
-    constructor(n,model,capacity,engineNo){
-        this.name= n;
-        this.model = model;
-        this.capacity=capacity;
-        this.engineNo = engineNo
-    }
-    showdetail(){
-        console.log(`Name: ${this.name}
-Model: ${this.model}
-Capacity: ${this.capacity}
-Engine No: ${this.engineNo}        
-`)
-    }
-}
+// class car{
+//     constructor(n,model,capacity,engineNo){
+//         this.name= n;
+//         this.model = model;
+//         this.capacity=capacity;
+//         this.engineNo = engineNo
+//     }
+//     showdetail(){
+//         console.log(`Name: ${this.name}
+// Model: ${this.model}
+// Capacity: ${this.capacity}
+// Engine No: ${this.engineNo}        
+// `)
+//     }
+// }
 // let c1 = new car("Alto","xyz",4,10010);
 // c1.showdetail();
 
-class Tesla extends car{
-    constructor(n,model,capacity,engineNo,fuelConsumption){
-        super(n,model,capacity,engineNo);
-        this.fuel = fuelConsumption;
+// class Tesla extends car{
+//     constructor(n,model,capacity,engineNo,fuelConsumption){
+//         super(n,model,capacity,engineNo);
+//         this.fuel = fuelConsumption;
+//     }
+//     showdetail(){
+//         console.log(`Name: ${this.name}
+// Model: ${this.model}
+// Capacity: ${this.capacity}
+// Engine No: ${this.engineNo}
+// Fuel Comsumption: ${this.fuel}        
+// `)
+// }
+// }
+// let t1 = new Tesla("xyz","abc", 4 , 53456,"electricity");
+// t1.showdetail();
+
+class Animals{
+    constructor(n,eat,weight){
+        this.name = n;
+        this.eat = eat;
+        this.weight  = weight
     }
     showdetail(){
         console.log(`Name: ${this.name}
-Model: ${this.model}
-Capacity: ${this.capacity}
-Engine No: ${this.engineNo}
-Fuel Comsumption: ${this.fuel}        
+Eat: ${this.eat}
+Weight: ${this.weight}
 `)
+    }
 }
+// let a1 = new Animals("Parrot","seed","0.2kg");
+// a1.showdetail();
+class Cow extends Animals{
+    constructor(n,eat,weight,place){
+        super(n,eat,weight);
+        this.place = place;
+    }
+    showdetail(){
+        console.log(`Name: ${this.name}
+Eat: ${this.eat}
+Weight: ${this.weight}
+Place: ${this.place}
+`)
+    }
 }
-let t1 = new Tesla("xyz","abc", 4 , 53456,"electricity");
-t1.showdetail();
+let c1 = new Cow("cow","Grass","2mon","Sibbi");
+c1.showdetail();
 
