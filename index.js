a993d60ed7b32d090187f8eb2266d63cba17894e
sadiali97 <sadiaali624726@gmail.com>
2022-07-20 10:25:41 +0500

    Update

--- index.js
+++ index.js
@@ -13,5 +13,23 @@ Engine No: ${this.engineNo}
 `)
     }
 }
-let c1 = new car("Alto","xyz",4,10010);
-c1.showdetail();
+// let c1 = new car("Alto","xyz",4,10010);
+// c1.showdetail();
+
+class Tesla extends car{
+    constructor(n,model,capacity,engineNo,fuelConsumption){
+        super(n,model,capacity,engineNo);
+        this.fuel = fuelConsumption;
+    }
+    showdetail(){
+        console.log(`Name: ${this.name}
+Model: ${this.model}
+Capacity: ${this.capacity}
+Engine No: ${this.engineNo}
+Fuel Comsumption: ${this.fuel}        
+`)
+}
+}
+let t1 = new Tesla("xyz","abc", 4 , 53456,"electricity");
+t1.showdetail();
+
